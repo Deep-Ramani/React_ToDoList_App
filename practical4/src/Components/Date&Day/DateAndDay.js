766bdec39ca71cce8dba38cb2263c24809f02f2e
Deep-Ramani <deepramani8505@gmail.com>
2022-02-16 18:15:03 +0530

    Feat:Change UI In DateDay Component

--- practical4/src/Components/Date&Day/DateAndDay.js
+++ practical4/src/Components/Date&Day/DateAndDay.js
@@ -5,10 +5,17 @@ function DateAndDay(){
         <>
         <div className="row dateday mt-5">
             {/* <div className="col"></div> */}
-            <div className="col-1 date">12</div>
-            <div className="col-1 monthyear">Jan <br/> 2019</div>
-            <div className="col-1 offset-9 day">Tuesday</div>
+            <div className="col">
+                <div className="row">
+            <div className="col date">12</div>
+            <div className="col monthyear">Jan <br/> 2019</div>
             <div className="col"></div>
+            <div className="col"></div>
+            
+            </div>
+            </div>
+            <div className="col day">Tuesday</div>
+            {/* <div className="col"></div> */}
         </div>
         </>
     );
