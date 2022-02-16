84e2afb967721a59f8a7b4bf27b0abe4776026c5
Deep-Ramani <deepramani8505@gmail.com>
2022-02-16 18:14:25 +0530

    Feat:Import List Component In TodoList File

--- practical4/src/Components/ToDoList.js
+++ practical4/src/Components/ToDoList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './ToDoList.css'
 import DateAndDay from "./Date&Day/DateAndDay";
-
+import List from "./List/List";
 function ToDoList(){
-    return(
+ 
+  return(
         <>
         <div className="container">
           <DateAndDay/>
+          <List/>
         </div>
         </>
     );
