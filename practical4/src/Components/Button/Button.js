9f23fa79459dd7037907f3a1100afd69bb2cac10
Deep-Ramani <deepramani8505@gmail.com>
2022-02-17 12:27:06 +0530

    Feat:Add Button Component

---
+++ practical4/src/Components/Button/Button.js
@@ -0,0 +1,12 @@
+import './Button.css'
+
+function Button(){
+    return(
+        
+        <div className='button'>
+            <button className='buttonplus'>+</button>
+        </div>
+    );
+}
+
+export default Button;
