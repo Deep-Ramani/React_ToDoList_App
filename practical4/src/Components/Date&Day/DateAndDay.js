f195517856e771e27cb0f6246da564aa5ca0d989
Deep-Ramani <deepramani8505@gmail.com>
2022-02-24 20:24:03 +0530

    feat:Adding Date Functionality

--- practical4/src/Components/Date&Day/DateAndDay.js
+++ practical4/src/Components/Date&Day/DateAndDay.js
@@ -3,18 +3,22 @@ import './DateAndDay.css'
 
 class DateAndDay extends React.Component{
     render() {
+        const dateToday = new Date();
+        const dateMonth = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+        const dateDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        
         return( 
         
         <div className="row dateday mt-5">
             <div className="col">
                 <div className="row">
-                    <div className="col date">12</div>
-                    <div className="col monthyear"><b>JAN</b> <br/> 2016</div>
+                    <div className="col date">{dateToday.getDate()}</div>
+                    <div className="col monthyear">{dateMonth[dateToday.getMonth()]} <br/> {dateToday.getFullYear()}</div>
                     <div className="col"></div>
                     <div className="col"></div>
                 </div>
             </div>
-                 <div className="col day">TUESDAY</div>
+                 <div className="col day">{dateDays[dateToday.getDay()].toUpperCase()}</div>
         </div>
        ); 
     }
