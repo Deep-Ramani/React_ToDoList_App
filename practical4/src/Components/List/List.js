3bc54501dcb9dd5bad29469440dbaa07bbd5f05e
Deep-Ramani <deepramani8505@gmail.com>
2022-02-24 20:08:05 +0530

    Feat: Created List Component

--- practical4/src/Components/List/List.js
+++ practical4/src/Components/List/List.js
@@ -1,45 +1,96 @@
-import { BsFillCheckCircleFill } from 'react-icons/bs';
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
+import ListInput from './InputList/ListInput';
 import ListItem from './ListItem/ListItem';
+import './List.css'
+
+
+
+const LocalItems = () => {
+    let list = localStorage.getItem('ListData');
+
+    if (list) {
+        return JSON.parse(localStorage.getItem('ListData'));
+    } else {
+        return [];
+    }
+}
+
+const dateCreated = () => {
+
+    let expDate = localStorage.getItem('expires');
+    if (expDate) {
+        return expDate;
+    } else {
+        return '';
+    }
+}
+
 
 
 function List(props){
-    const listItem =[
-        {
-          listdetail : 'Buy New SweatShirts',
-          icon : <BsFillCheckCircleFill/>
-        },
-        {
-            listdetail : 'Begin Promotional Phase',
-        },
-        {
-            listdetail : 'Read an article',
-        },
-        {
-            listdetail : 'Try not to fall asleep',
-        },
-        {
-            listdetail : 'Watch \'Sherlock\'',
-        },
-        {
-            listdetail : 'Begin QA for the product',
-        },
-        {
-            listdetail : 'Go for the Walk',
-        },
-      ]
 
+    const today = new Date();
+    const date = today.getFullYear() + '-' + (today.getMonth()) + '-' + (today.getDate());
+    const expiry = today.getFullYear() + '-' + (today.getMonth()) + '-' + (today.getDate());
+    const expires = dateCreated();
+    const[ListData, setListData] =useState(LocalItems());
+    
+    useEffect(() => {
+        if (date > expires) {
+            console.log("List removed");
+            setListData([]);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
+
+    useEffect(() => {
+        if (ListData.length === 0) {
+            console.log("setted expDate");
+            localStorage.setItem('expires', expiry);
+        } else if (!expires){
+            console.log("setted expDate");
+            localStorage.setItem('expires', expiry);
+        }
+        localStorage.setItem('ListData', JSON.stringify(ListData));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ListData,expiry]);
+
+    const toggleCheck = (index) => {
+        let newTodos = [...ListData];
+        if (newTodos[index].checked) {
+            newTodos[index].checked = false;
+        }else{
+            newTodos[index].checked = true;
+        }
+        setListData(newTodos);
+    }
+
+
+    const addTodo = (listdetail) => {
+        setListData([...ListData,{listdetail,checked: false}]);
+    }
+    
     return(
-    <>
-        <ListItem completed={true} listdetail={listItem[0].listdetail} icon={listItem[0].icon}/>
+        <>
+    <div className='List'>
+     {ListData.map((todo,index) => {
+       return <ListItem key={index} index={index} todo={todo} toggleCheck={toggleCheck}   />
+     })}
+   </div>
+   <ListInput hideButton={props.hideButton} toggleHideButton={props.toggleHideButton} addTodo={addTodo}/>
+   
+
+        {/* <ListItem completed={true} listdetail={listItem[0].listdetail} icon={listItem[0].icon}/>
         <ListItem completed={true} listdetail={listItem[1].listdetail} icon={listItem[0].icon}/>
         <ListItem listdetail={listItem[2].listdetail}/>
         <ListItem listdetail={listItem[3].listdetail}/>
         <ListItem listdetail={listItem[4].listdetail}/>
         <ListItem listdetail={listItem[5].listdetail}/>
-        <ListItem listdetail={listItem[6].listdetail}/>
-    </>
-    );
-}
+        <ListItem listdetail={listItem[6].listdetail}/> */}
+       
+      
+       </> 
+    );}
 
 export default List;
